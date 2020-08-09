885b5c83540a2064a30b5375acc773282d0eedd4
Andrzejewski Kamil <kamil@andrzejewski.dev>
2020-08-09 12:45:37 +0200

    [G4G-93] User profile

--- src/middlewares/auth.handler.ts
+++ src/middlewares/auth.handler.ts
@@ -2,28 +2,32 @@ import passport from "passport";
 import { ForbiddenException } from "../constants/exceptions";
 
 export const authorize = passport.authenticate('local', {
-  failureRedirect: '/login',
-  failureFlash: "Invalid username or password."
+    failureRedirect: '/login',
+    failureFlash: "Invalid username or password."
 });
 
 export const authorizeApi = passport.authenticate('local', {
-  session: false
+    session: false
 });
 
 export function checkAuthenticated(req, res, next) {
-    passport.authenticate("headerapikey")(req, res, function() {
-      if (req.user) {
-         return next();
-      }
+    if (req.user) {
+        return next();
+    }
 
-      next(new ForbiddenException());
+    passport.authenticate("headerapikey")(req, res, function() {
+        if (req.user) {
+            return next();
+        }
+
+        next(new ForbiddenException());
     });
 }
 
 export function checkNotAuthenticated(req, res, next) {
-  if (req.user) {
-    next(new ForbiddenException());
-  }
+    if (req.user) {
+        next(new ForbiddenException());
+    }
 
-  next();
+    next();
 }
