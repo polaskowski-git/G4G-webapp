692c5cb746a7d7015725085264803f38ddd34361
Andrzejewski Kamil <kamil@andrzejewski.dev>
2020-09-06 12:29:17 +0200

    part 2

--- src/models/rankings.models.ts
+++ src/models/rankings.models.ts
@@ -1,14 +1,21 @@
 import { ApiModel } from "swagger-express-ts";
 import { ApiModelProperty } from "../constants/decorators";
 import { BaseModel } from "./base.model";
-import { UserModel } from "./user.models";
+import User from "../entities/user.entity";
+import { interfaces } from "inversify";
 
 @ApiModel({
-    name: RankingModel.NAME
+    name: RankingElementModel.NAME
 })
-export class RankingModel extends BaseModel<RankingModel>
+export class RankingElementModel extends BaseModel<RankingElementModel>
 {
-    public static readonly NAME = "RankingModel";
+    public static readonly NAME = "RankingElement";
+
+    @ApiModelProperty({
+        required: true,
+        type: 'integer'
+    })
+    rank: number;
 
     @ApiModelProperty({
         required: true,
@@ -18,8 +25,29 @@ export class RankingModel extends BaseModel<RankingModel>
 
     @ApiModelProperty({
         required: true,
-        type: "integer"
+        type: "number"
     })
     score: number;
 
+}
+
+@ApiModel({
+    name: RankingModel.NAME
+})
+export class RankingModel extends BaseModel<RankingModel>
+{
+    public static readonly NAME = "Ranking";
+
+    @ApiModelProperty({
+        required: true,
+        model: RankingElementModel.NAME
+    })
+    userElement: RankingElementModel;
+
+    @ApiModelProperty({
+        required: true,
+        type: "array",
+        model: RankingElementModel.NAME
+    })
+    elements: RankingElementModel[];
 }
